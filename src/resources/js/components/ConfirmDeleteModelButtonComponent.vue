7487a41e0fc168a8fac648db7eaa0b75f8d49e37
Vladimir Peshkov <vladimirpeshkov5@gmail.com>
2019-04-24 21:38:03 +0300

    change confirm button

--- src/resources/js/components/ConfirmDeleteModelButtonComponent.vue
+++ src/resources/js/components/ConfirmDeleteModelButtonComponent.vue
@@ -1,31 +1,29 @@
 <template>
-    <div v-if="showConfirm">
-        <slot name="delete"></slot>
-        <div class="btn-group">
+    <div class="btn-group">
+        <template v-if="showConfirm">
             <button class="btn btn-danger"
                     @click="submitForm()">
                 <i class="far fa-check-circle"></i>
             </button>
+            <slot name="delete"></slot>
             <button type="button"
                     @click="toggleConfirm()"
                     class="btn btn-success">
                 <i class="fas fa-ban"></i>
             </button>
-        </div>
-    </div>
-    <div class="btn-group"
-         role="group"
-         v-else>
-        <slot name="edit"></slot>
-        <slot name="show"></slot>
-        <slot name="other"></slot>
-        <button type="button"
-                v-if="hasSlot()"
-                @click="toggleConfirm()"
-                class="btn btn-danger">
-            <i class="fas fa-trash-alt"></i>
-        </button>
-    </div>
+        </template>
+        <template v-else>
+            <slot name="edit"></slot>
+            <slot name="show"></slot>
+            <slot name="other"></slot>
+            <button type="button"
+                    v-if="hasSlot()"
+                    @click="toggleConfirm()"
+                    class="btn btn-danger">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+        </template>
+    </div class="btn-group">
 </template>
 
 <script>
