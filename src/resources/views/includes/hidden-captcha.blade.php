05e284fa07e8a8439e69f4109fdac94b9328990c
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-02-18 10:04:53 +0300

    change includes

--- src/resources/views/includes/hidden-captcha.blade.php
+++ src/resources/views/includes/hidden-captcha.blade.php
@@ -1,7 +1,5 @@
 <div class="form-group" style="display: none">
-    <label for="geo_check">Check</label>
     <input type="text"
-           id="geo_check"
            name="geo_check"
            placeholder="Enter check"
            value="{{ old('geo_check') }}"
