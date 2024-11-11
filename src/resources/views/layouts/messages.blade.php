fc4fabae0eea0cab68e08c010c2d16a3f3db2d1d
Nataliess <n.borisova@me.com>
2024-11-11 16:51:33 +0300

    messages btn-close

--- src/resources/views/layouts/messages.blade.php
+++ src/resources/views/layouts/messages.blade.php
@@ -1,24 +1,21 @@
 @if(session('danger'))
     <div class="alert alert-danger d-flex justify-content-between" role="alert">
         {{ session('danger') }}
-        <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
         </button>
     </div>
 @endif
 @if(session('success'))
     <div class="alert alert-success d-flex justify-content-between" role="alert">
         {{ session('success') }}
-        <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
         </button>
     </div>
 @endif
 @if(session('status'))
     <div class="alert alert-primary d-flex justify-content-between" role="alert">
         {{ session('status') }}
-        <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
         </button>
     </div>
 @endif
