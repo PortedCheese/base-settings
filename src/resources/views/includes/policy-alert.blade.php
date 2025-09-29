a96bf83fb61b5f9e363933e09cb42436509ce007
Nataliess <n.borisova@me.com>
2025-09-29 12:24:43 +0300

    fix

--- src/resources/views/includes/policy-alert.blade.php
+++ src/resources/views/includes/policy-alert.blade.php
@@ -58,7 +58,7 @@
     <button type="button" class="position-absolute top-0 end-0 btn btn-outline-primary m-3 me-2 alert-default__close" data-bs-dismiss="alert" aria-label="Close"> <i class="fas fa-check"></i></button>
     <div class="alert-default__body">
         <small>
-            Наш сайт использует файлы cookie, чтобы улучшить работу сайта, повысить его эффективность и удобство. Продолжая использовать сайт <a href="{{ route("home") }}">{{ config("app.name") }}</a>, я даю свое сгласие {{ config("policy.company","") }} на обработку моих персональных данных с использованием сервиса веб-аналитики Яндекс Метрика. Ознакомлен с <a href="{{ route("policy") }}">Политикой обработки персональных данных</a>.
+            Наш сайт использует файлы cookie, чтобы улучшить работу сайта, повысить его эффективность и удобство. Продолжая использовать сайт <a href="{{ route("home") }}">{{ config("app.name") }}</a>, я даю свое согласие {{ config("policy.company","") }} на обработку моих персональных данных с использованием сервиса веб-аналитики Яндекс Метрика. Ознакомлен с <a href="{{ route("policy") }}">Политикой обработки персональных данных</a>.
         </small>
     </div>
 </div>
